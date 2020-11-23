4d5842529a64564c2218f2d69d8d120939669e7b
1000channel <ponnge@gmail.com>
2020-11-23 19:39:45 +0900

    turbolinksの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
+//turbolinksが読み込まれないように編集・無効化
 require("@rails/activestorage").start()
 require("channels")
 
