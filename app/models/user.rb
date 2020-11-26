315115fd6d599b60e06e6f752dd85aabcb3449ae
1000channel <ponnge@gmail.com>
2020-11-26 12:18:17 +0900

    ビューの配置・バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+    validates :birth_date
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
+    validates :last_name
+    validates :first_name
+  end
+
+
+  has_many :hair_styles
+  has_one :shop
+
 end
